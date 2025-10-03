a21d8ace56a2c6f7d156a163f2928ca78f4279c4
Alexander Cartaya <alexandercartaya96@gmail.com>
2025-10-03 11:48:42 -0400

    added a createsuperuse.py to automate superuser for render.com

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -6,16 +6,6 @@ python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
-  echo "Checking for superuser..."
-  python manage.py shell <<EOF
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username="$DJANGO_SUPERUSER_USERNAME").exists():
-    User.objects.create_superuser("$DJANGO_SUPERUSER_USERNAME", "$DJANGO_SUPERUSER_EMAIL", "$DJANGO_SUPERUSER_PASSWORD")
-    print("Superuser created.")
-else:
-    print("Superuser already exists.")
-EOF
+python manage.py createsuperuser --admin admin2 --password mysecretadminpassword
 
 exec "$@"
