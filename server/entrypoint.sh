ef4adf7e6bc443bebd06ede4fa9ca4dd5e88bee7
Alexander Cartaya <alexandercartaya96@gmail.com>
2025-10-03 11:27:33 -0400

    Deployment take 2

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+  echo "Checking for superuser..."
+  python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="$DJANGO_SUPERUSER_USERNAME").exists():
+    User.objects.create_superuser("$DJANGO_SUPERUSER_USERNAME", "$DJANGO_SUPERUSER_EMAIL", "$DJANGO_SUPERUSER_PASSWORD")
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
+EOF
 # Make migrations and migrate the database.
 echo "Making migrations and migrating the database. "
 python manage.py makemigrations --noinput
