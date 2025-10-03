5d04cf6cfa77e2446860bad58556145b15e5e8a2
Alexander Cartaya <alexandercartaya96@gmail.com>
2025-10-03 11:33:41 -0400

    Update entrypoint.sh

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+# Make migrations and migrate the database.
+echo "Making migrations and migrating the database. "
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
   echo "Checking for superuser..."
   python manage.py shell <<EOF
@@ -10,9 +17,5 @@ if not User.objects.filter(username="$DJANGO_SUPERUSER_USERNAME").exists():
 else:
     print("Superuser already exists.")
 EOF
-# Make migrations and migrate the database.
-echo "Making migrations and migrating the database. "
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+
 exec "$@"
