ef58c8337412367fabdd703fd026219c4d8c134b
Alexander Cartaya <alexandercartaya96@gmail.com>
2025-10-05 15:46:39 -0400

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 
@@ -86,11 +87,14 @@ WSGI_APPLICATION = "djangoproj.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(
+        os.environ.get(
+            "DATABASE_URL",
+            "sqlite:///{}".format(BASE_DIR / "db.sqlite3"),
+        )
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
