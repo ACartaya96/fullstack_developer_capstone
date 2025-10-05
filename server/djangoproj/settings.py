914ea5a53beb811236dc0906cf82a1bb21c2e12b
Alexander Cartaya <alexandercartaya96@gmail.com>
2025-10-05 16:03:47 -0400

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = (
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://fullstack-developer-capstone-1.onrender.com"]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [],
